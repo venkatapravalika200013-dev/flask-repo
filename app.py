841087e02438e99a015dfd066c9a7f3a2cc8b65b
venkatapravalika200013-dev <venkatapravalika200013@gmail.com>
2025-10-25 12:14:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-  return " Flask CI/CD from Github to AWS works"
+  return " Flask CI/CD from Github to AWS works "
 
 if __name__ == '__main__':
        app.run(host='0.0.0.0',port=5000)
